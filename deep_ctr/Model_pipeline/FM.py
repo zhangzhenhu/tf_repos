6b9ce7967ae91bb99a5152060ee05ecd5e2cc07e
张振虎 <zhangzhenhu1@100tal.com>
2019-03-27 12:53:15 +0800

    add FM

--- deep_ctr/Model_pipeline/FM.py
+++ deep_ctr/Model_pipeline/FM.py
@@ -15,11 +15,12 @@ by lambdaji
 
 # import argparse
 import shutil
-# import sys
+import sys
 import os
 import json
 import glob
 from datetime import date, timedelta
+
 from time import time
 # import gc
 # from multiprocessing import Process
@@ -29,6 +30,7 @@ import random
 # import pandas as pd
 # import numpy as np
 import tensorflow as tf
+from tensorflow.python import debug as tf_debug
 
 #################### CMD Arguments ####################
 FLAGS = tf.app.flags.FLAGS
@@ -38,11 +40,11 @@ tf.app.flags.DEFINE_string("worker_hosts", '', "Comma-separated list of hostname
 tf.app.flags.DEFINE_string("job_name", '', "One of 'ps', 'worker'")
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
 tf.app.flags.DEFINE_integer("num_threads", 16, "Number of threads")
-tf.app.flags.DEFINE_integer("feature_size", 0, "Number of features")
-tf.app.flags.DEFINE_integer("field_size", 0, "Number of fields")
+tf.app.flags.DEFINE_integer("feature_size", 117581, "Number of features")
+tf.app.flags.DEFINE_integer("field_size", 39, "Number of fields")
 tf.app.flags.DEFINE_integer("embedding_size", 32, "Embedding size")
-tf.app.flags.DEFINE_integer("num_epochs", 10, "Number of epochs")
-tf.app.flags.DEFINE_integer("batch_size", 64, "Number of batch size")
+tf.app.flags.DEFINE_integer("num_epochs", 1, "Number of epochs")
+tf.app.flags.DEFINE_integer("batch_size", 2048, "Number of batch size")
 tf.app.flags.DEFINE_integer("log_steps", 1000, "save summary every steps")
 tf.app.flags.DEFINE_float("learning_rate", 0.0005, "learning rate")
 tf.app.flags.DEFINE_float("l2_reg", 0.0001, "L2 regularization")
@@ -52,9 +54,9 @@ tf.app.flags.DEFINE_string("deep_layers", '256,128,64', "deep layers")
 tf.app.flags.DEFINE_string("dropout", '0.5,0.5,0.5', "dropout rate")
 tf.app.flags.DEFINE_boolean("batch_norm", False, "perform batch normaization (True or False)")
 tf.app.flags.DEFINE_float("batch_norm_decay", 0.9, "decay for the moving average(recommend trying decay=0.9)")
-tf.app.flags.DEFINE_string("data_dir", '', "data dir")
+tf.app.flags.DEFINE_string("data_dir", '../data/', "data dir")
 tf.app.flags.DEFINE_string("dt_dir", '', "data dt partition")
-tf.app.flags.DEFINE_string("model_dir", '', "model check point dir")
+tf.app.flags.DEFINE_string("model_dir", '../models/FM', "model check point dir")
 tf.app.flags.DEFINE_string("servable_model_dir", '', "export servable model for TensorFlow Serving")
 tf.app.flags.DEFINE_string("task_type", 'train', "task type {train, infer, eval, export}")
 tf.app.flags.DEFINE_boolean("clear_existing_model", False, "clear existing model or not")
@@ -68,6 +70,8 @@ def input_fn(filenames, batch_size=32, num_epochs=1, perform_shuffle=False):
         # columns = tf.decode_csv(value, record_defaults=CSV_COLUMN_DEFAULTS)
         # features = dict(zip(CSV_COLUMNS, columns))
         # labels = features.pop(LABEL_COLUMN)
+        # print(line.eval())
+        # tf.Print(line,[line,line.shape])
         columns = tf.string_split([line], ' ')
         labels = tf.string_to_number(columns.values[0], out_type=tf.float32)
         splits = tf.string_split(columns.values[1:], ':')
@@ -80,6 +84,7 @@ def input_fn(filenames, batch_size=32, num_epochs=1, perform_shuffle=False):
         #    feat_ids.append(tf.string_to_number(splits.values[2*i], out_type=tf.int32))
         #    feat_vals.append(tf.string_to_number(splits.values[2*i+1]))
         # return tf.reshape(feat_ids,shape=[-1,field_size]), tf.reshape(feat_vals,shape=[-1,field_size]), labels
+        # tf.Print(feat_ids,[feat_ids,feat_ids])
         return {"feat_ids": feat_ids, "feat_vals": feat_vals}, labels
 
     # Extract lines from input files using the Dataset API, can pass one filename or filename list
@@ -121,17 +126,20 @@ def model_fn(features, labels, mode, params):
                            initializer=tf.glorot_normal_initializer())
 
     # ------build feaure-------
+    # 其实只有 field_size=39 个原始特征，通过one-hot扩展成了 feature_size=117581 个特征
+    # 稀疏方法下，最终实际有值的只有 field_size 个特征
     feat_ids = features['feat_ids']
-    feat_ids = tf.reshape(feat_ids, shape=[-1, field_size])
+    feat_ids = tf.reshape(feat_ids, shape=[-1, field_size])  # shape=(batch_size,field_size)
     feat_vals = features['feat_vals']
-    feat_vals = tf.reshape(feat_vals, shape=[-1, field_size])
+    feat_vals = tf.reshape(feat_vals, shape=[-1, field_size])  # shape=(batch_size,field_size)
 
     # ------build f(x)------
-    with tf.variable_scope("First-order"):
-        feat_wgts = tf.nn.embedding_lookup(FM_W, feat_ids)  # None * F * 1
-        y_w = tf.reduce_sum(tf.multiply(feat_wgts, feat_vals), 1)
+    with tf.variable_scope("Linear-part"):
+        # 由于 feat_ids 采用稀疏的方式，利用 id 找到 FM_W 对应位置的权重值
+        feat_wgts = tf.nn.embedding_lookup(FM_W, feat_ids)  # shape=(batch_size,field_size)
+        y_w = tf.reduce_sum(tf.multiply(feat_wgts, feat_vals), 1)  # shape=(batch_size,)
 
-    with tf.variable_scope("Second-order"):
+    with tf.variable_scope("FM-part"):
         embeddings = tf.nn.embedding_lookup(FM_V, feat_ids)  # None * F * K
         feat_vals = tf.reshape(feat_vals, shape=[-1, field_size, 1])
         embeddings = tf.multiply(embeddings, feat_vals)  # vij*xi
@@ -139,49 +147,10 @@ def model_fn(features, labels, mode, params):
         square_sum = tf.reduce_sum(tf.square(embeddings), 1)
         y_v = 0.5 * tf.reduce_sum(tf.subtract(sum_square, square_sum), 1)  # None * 1
 
-    with tf.variable_scope("Deep-part"):
-        if FLAGS.batch_norm:
-            # normalizer_fn = tf.contrib.layers.batch_norm
-            # normalizer_fn = tf.layers.batch_normalization
-            if mode == tf.estimator.ModeKeys.TRAIN:
-                train_phase = True
-                # normalizer_params = {'decay': batch_norm_decay, 'center': True, 'scale': True, 'updates_collections': None, 'is_training': True, 'reuse': None}
-            else:
-                train_phase = False
-                # normalizer_params = {'decay': batch_norm_decay, 'center': True, 'scale': True, 'updates_collections': None, 'is_training': False, 'reuse': True}
-        else:
-            normalizer_fn = None
-            normalizer_params = None
-
-        deep_inputs = tf.reshape(embeddings, shape=[-1, field_size * embedding_size])  # None * (F*K)
-        for i in range(len(layers)):
-            # if FLAGS.batch_norm:
-            #    deep_inputs = batch_norm_layer(deep_inputs, train_phase=train_phase, scope_bn='bn_%d' %i)
-            # normalizer_params.update({'scope': 'bn_%d' %i})
-            deep_inputs = tf.contrib.layers.fully_connected(inputs=deep_inputs, num_outputs=layers[i], \
-                                                            # normalizer_fn=normalizer_fn, normalizer_params=normalizer_params, \
-                                                            weights_regularizer=tf.contrib.layers.l2_regularizer(
-                                                                l2_reg), scope='mlp%d' % i)
-            if FLAGS.batch_norm:
-                deep_inputs = batch_norm_layer(deep_inputs, train_phase=train_phase,
-                                               scope_bn='bn_%d' % i)  # 放在RELU之后 https://github.com/ducha-aiki/caffenet-benchmark/blob/master/batchnorm.md#bn----before-or-after-relu
-            if mode == tf.estimator.ModeKeys.TRAIN:
-                deep_inputs = tf.nn.dropout(deep_inputs, keep_prob=dropout[
-                    i])  # Apply Dropout after all BN layers and set dropout=0.8(drop_ratio=0.2)
-                # deep_inputs = tf.layers.dropout(inputs=deep_inputs, rate=dropout[i], training=mode == tf.estimator.ModeKeys.TRAIN)
-
-        y_deep = tf.contrib.layers.fully_connected(inputs=deep_inputs, num_outputs=1, activation_fn=tf.identity, \
-                                                   weights_regularizer=tf.contrib.layers.l2_regularizer(l2_reg),
-                                                   scope='deep_out')
-        y_d = tf.reshape(y_deep, shape=[-1])
-        # sig_wgts = tf.get_variable(name='sigmoid_weights', shape=[layers[-1]], initializer=tf.glorot_normal_initializer())
-        # sig_bias = tf.get_variable(name='sigmoid_bias', shape=[1], initializer=tf.constant_initializer(0.0))
-        # deep_out = tf.nn.xw_plus_b(deep_inputs,sig_wgts,sig_bias,name='deep_out')
-
-    with tf.variable_scope("DeepFM-out"):
+    with tf.variable_scope("FM-out"):
         # y_bias = FM_B * tf.ones_like(labels, dtype=tf.float32)  # None * 1  warning;这里不能用label，否则调用predict/export函数会出错，train/evaluate正常；初步判断estimator做了优化，用不到label时不传
-        y_bias = FM_B * tf.ones_like(y_d, dtype=tf.float32)  # None * 1
-        y = y_bias + y_w + y_v + y_d
+        # y_bias = FM_B * tf.ones_like(y_d, dtype=tf.float32)  # None * 1
+        y = FM_B + y_w + y_v  # + y_d
         pred = tf.sigmoid(y)
 
     predictions = {"prob": pred}
@@ -301,7 +270,7 @@ def main(_):
     # ------check Arguments------
     if FLAGS.dt_dir == "":
         FLAGS.dt_dir = (date.today() + timedelta(-1)).strftime('%Y%m%d')
-    FLAGS.model_dir = FLAGS.model_dir + FLAGS.dt_dir
+    # FLAGS.model_dir = FLAGS.model_dir + FLAGS.dt_dir
     # FLAGS.data_dir  = FLAGS.data_dir + FLAGS.dt_dir
 
     print('task_type ', FLAGS.task_type)
@@ -357,16 +326,27 @@ def main(_):
         log_step_count_steps=FLAGS.log_steps, save_summary_steps=FLAGS.log_steps)
     DeepFM = tf.estimator.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir, params=model_params, config=config)
 
+    # Create a LocalCLIDebugHook and use it as a monitor when calling fit().
+    # hooks = [tf_debug.LocalCLIDebugHook()]
+    # hooks = [tf_debug.TensorBoardDebugHook("zhangzhenhudeMacBook-Pro.local:6064")]
+
     if FLAGS.task_type == 'train':
         train_spec = tf.estimator.TrainSpec(
-            input_fn=lambda: input_fn(tr_files, num_epochs=FLAGS.num_epochs, batch_size=FLAGS.batch_size))
+            input_fn=lambda: input_fn(tr_files, num_epochs=FLAGS.num_epochs, batch_size=FLAGS.batch_size),
+            # hooks=hooks
+        )
         eval_spec = tf.estimator.EvalSpec(
             input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size), steps=None,
             start_delay_secs=1000, throttle_secs=1200)
+        # model = tf.estimator.Estimator(input_fn=)
+        # tf.estimator.train(DeepFM, train_spec,hooks=hooks)
         tf.estimator.train_and_evaluate(DeepFM, train_spec, eval_spec)
     elif FLAGS.task_type == 'eval':
+
         DeepFM.evaluate(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size))
+
     elif FLAGS.task_type == 'infer':
+
         preds = DeepFM.predict(input_fn=lambda: input_fn(te_files, num_epochs=1, batch_size=FLAGS.batch_size),
                                predict_keys="prob")
         with open(FLAGS.data_dir + "/pred.txt", "w") as fo:
@@ -388,5 +368,14 @@ def main(_):
 
 
 if __name__ == "__main__":
+    # sys.argv.append('--model_dir=../models/FM')
+    # sys.argv.append('--data_dir=../data/')
+    # sys.argv.append('--num_epochs=1')
+    # sys.argv.append('--embedding_size=32')
+    # sys.argv.append('--batch_size=2048')
+    # sys.argv.append('--field_size=39')
+    # sys.argv.append('--feature_size=117581')
+    # sys.argv.append('--task_type=eval')
+    # print(sys.argv)
     tf.logging.set_verbosity(tf.logging.INFO)
     tf.app.run()
---
+++ deep_ctr/Model_pipeline/test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# 
+#
+"""
+模块用途描述
+
+Authors: zhangzhenhu(acmtiger@gmail.com)
+Date:    2019-03-26 16:17
+"""
+import sys
+import argparse
+
+__version__ = 1.0
+
+
+def init_option():
+    """
+    初始化命令行参数项
+    Returns:
+        OptionParser 的parser对象
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", dest="input",
+                        help=u"输入文件；默认标准输入设备")
+    parser.add_argument("-o", "--output", dest="output",
+                        help=u"输出文件；默认标准输出设备")
+    return parser
+
+
+def main(options):
+    pass
+
+
+if __name__ == "__main__":
+
+    parser = init_option()
+    options = parser.parse_args()
+
+    if options.input:
+
+        options.input = open(options.input)
+    else:
+        options.input = sys.stdin
+    main(options)
